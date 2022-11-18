ff32c394d02e9a5d69c1b831f044308fd5aee35b
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-11-18 20:34:34 +0100

    readfile && savefile

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Include all needed modules
 const express = require('express');
 const cors = require('cors');
-//var jsonfile2 = require('jsonfile');
+var jsonfile = require('jsonfile');
 
 
 // Create an Express application
@@ -18,20 +18,20 @@ app.listen(port, function() {
 
 
 // Read from route
-var miundb = require('./miun-db.json');
+//var miundb = require('./miun-db.json');
 
 // Read from file
-//var file = "miun-db.json";
-//var miundb = [];
+var file = "miun-db.json";
+var miundb = [];
 
-//jsonfile2.readFile(file, function(err, obj) {
-//    if (err) {
-//        console.log(err);
-//    } else {
-//        console.log(obj);
-//        miundb = obj;
-//    }
-//});
+jsonfile.readFile(file, function(err, obj) {
+    if (err) {
+        console.log(err);
+    } else {
+        //console.log(obj); // TODO: Remove
+        miundb = obj;
+    }
+});
 
 
 app.get('/api/courses', function(req, res) {
@@ -98,12 +98,18 @@ app.get('/api/courses/my', function(req, res) { //Funkar inte? HMM?????
     // en fuling, ta alla miundb och jämför med mina osv :P
 }); 
 
-// Hämta alla ämnen
+app.get('/api/courses/my/:courseCode', function(req, res) {
+    var code = req.params.courseCode;
+
+    var send = {};
+})
+
+// Get all subjects
 app.get('/api/subjects', function(req, res) {
     res.send(miundb.subjects);
 })
 
-// Hämta ett specifikt ämne
+// Get a specific subject
 app.get('/api/subjects/:subjectCode', function(req, res) {
     // Gets the subjectcode
     var subcode = req.params.subjectCode;
@@ -129,10 +135,62 @@ app.get('/api/subjects/:subjectCode', function(req, res) {
     
 })
 
+// Add a new MyCourse
+app.post('/api/courses/my', function(req, res) {
+
+    // If courseCode not in miundb
+
+        // Return error msg && htpp404
+
+    // Else
+
+        // If course in MyCourses
+
+            // Return error msg && http 409
+
+        // Else
+
+            // Save the course, return all data && http 201
+});
+
+// Update a My-course
+app.put('/api/courses/my/:courseCode', function(req, res) {
+
+    var code = req.params.courseCode;
+
+    // If in MyCourses
+
+        // Update grade
+
+        // Return MyCourse
+
+});
+
+// Delete a My-course
+app.delete('/api/courses/my/:courseCode', function(req, res) {
+
+    var code = req.params.courseCode;
+
+    // If in myCourses
+
+        // Delete the course && return the course with all its data
+
+    // If not in myCourses
+
+        // Return error msg && HTTP 404
+
+});
+
+// Get all grades
+app.get('/api/grades', function(req, res) {
+
+});
+
+// Save JSON file
 function saveFile() {
-    jsonfile2.writeFile(file, miundb, function(err) {
+    jsonfile.writeFile(file, miundb, function(err) {
         console.log(err);
     });
-}
+};
 
 
