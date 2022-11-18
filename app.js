de1fb202f1879fbedff184e2f4e1fb44c79c166e
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-11-18 21:33:20 +0100

    get MyCourses:courseCode

--- app.js
+++ app.js
@@ -88,15 +88,17 @@ app.get('/api/courses/:courseCode', function(req, res) {
 
 */
 app.get('/api/courses/my', function(req, res) { //Funkar inte? HMM?????
-    
-    
-    
+        
+    // Get myCourses from db
     for (course of miundb.myCourses) {
 
+        // Get all courses
         for (miuncourse of miundb.courses) {
-
+            
+            // Compares with coursecode
             if (course.courseCode == miuncourse.courseCode) {
                 
+                // Adds data from miudb.courses to myCourses
                 miuncourse["grade"] == course.grade;
                 course["subjectCode"] = miuncourse.subjectCode;
                 course["level"] = miuncourse.level;
@@ -105,8 +107,10 @@ app.get('/api/courses/my', function(req, res) { //Funkar inte? HMM?????
                 course["points"] = miuncourse.points;
                 course["institutionCode"] = miuncourse.institutionCode;
                 
+                // Get the subjects
                 for(subject of miundb.subjects) {
-
+                    
+                    // Adds subject to myCourses if correct subjectcode
                     if (subject.subjectCode == course.subjectCode) {
                         course["subject"] = subject.subject;
                     }
@@ -116,16 +120,60 @@ app.get('/api/courses/my', function(req, res) { //Funkar inte? HMM?????
             
         };
     };
-    //res.send({ "message": "Hello, World!" });
-    //res.status(200).json(miundb);
+
     res.send(miundb.myCourses);
-    // en fuling, ta alla miundb och jämför med mina osv :P
+    
 }); 
 
 app.get('/api/courses/my/:courseCode', function(req, res) {
+    
     var code = req.params.courseCode;
-
     var send = {};
+    
+    // Get myCourses from db
+    for (course of miundb.myCourses) {
+
+        // If correct coursecode
+        if (course.courseCode == code.toUpperCase()) {            
+        
+            // Get all courses
+            for (miuncourse of miundb.courses) {
+                
+                // Compares with coursecode
+                if (course.courseCode == miuncourse.courseCode) {
+                    
+                    // Adds data from miudb.courses to myCourses
+                    miuncourse["grade"] == course.grade;
+                    course["subjectCode"] = miuncourse.subjectCode;
+                    course["level"] = miuncourse.level;
+                    course["progression"] = miuncourse.progression;
+                    course["name"] = miuncourse.name;
+                    course["points"] = miuncourse.points;
+                    course["institutionCode"] = miuncourse.institutionCode;
+                    
+                    // Get the subjects
+                    for(subject of miundb.subjects) {
+                        
+                        // Adds subject to myCourses if correct subjectcode
+                        if (subject.subjectCode == course.subjectCode) {
+                            course["subject"] = subject.subject;
+                        }
+                        // Sends the object
+                        send = course;
+                    }
+                    
+                };
+                
+            };
+
+        } else {
+            send
+        };
+    };
+
+    res.send(send);
+
+
 })
 
 // Get all subjects
